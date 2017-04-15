7ee30fab8d112d919b45917a25d45224673c3d3f
abhishek0mukherjee <abhishek0mukherjee@gmail.com>
2017-04-15 13:05:57 -0400

    add phase 4

--- server.js
+++ server.js
@@ -22,27 +22,62 @@ var table = [{
   display2: reservationarray
 }];
 
-app.get("/", function(req, res) {
-  res.send("Welcome!");
+// app.get("/", function(req, res) {
+//   res.send("Welcome!");
+// });
+
+// app.get("/api/:table?", function(req, res) {
+//   var chosen = req.params.table;
+
+//   if (chosen) {
+//     console.log(chosen);
+
+//     for (var i = 0; i < table.length; i++) {
+//       if (chosen === table[i].routeName) {
+//         res.json(table[i]);
+//         return;
+//       }
+//     }
+
+//     return res.send("Not a valid path");
+//   }
+//   return res.json(table);
+// });
+
+app.get("/", function(req, res) 
+{
+  res.sendFile(path.join(__dirname, "home.html"));
 });
 
-app.get("/api/:table?", function(req, res) {
-  var chosen = req.params.table;
-
-  if (chosen) {
-    console.log(chosen);
-
-    for (var i = 0; i < table.length; i++) {
-      if (chosen === table[i].routeName) {
-        res.json(table[i]);
-        return;
-      }
-    }
-
-    return res.send("Not a valid path");
-  }
-  return res.json(table);
+app.get("/tables", function(req, res) 
+{
+  res.sendFile(path.join(__dirname, "tables.html"));
 });
+
+app.get("/reservations", function(req, res) 
+{
+  res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+app.get("/api/reservations", function(req, res)
+{
+	return res.json(reservations);
+});
+
+
+app.post("/reservations/new", function(res, req)
+{
+	var newRes = req.body;
+	newRes.routeName = newRes.name.replace(/\s+/g, "").toLowerCase();
+
+	console.log(newRes);
+
+	reservations.push(newRes);
+
+	res.json(newRes);
+});
+
+
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
