dc2087f2c4c16030671d08d6d627318a88c836da
abhishek0mukherjee <abhishek0mukherjee@gmail.com>
2017-04-15 11:42:12 -0400

    routes added basic

--- server.js
+++ server.js
@@ -12,12 +12,37 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
+var tablesarray = ["test"];
+
+var table = [{
+  routeName: "table",
+  name: "table",
+  display: tablesarray
+}];
 
 app.get("/", function(req, res) {
   res.send("Welcome!");
 });
 
+app.get("/api/:table?", function(req, res) {
+  var chosen = req.params.table;
+
+  if (chosen) {
+    console.log(chosen);
+
+    for (var i = 0; i < table.length; i++) {
+      if (chosen === table[i].routeName) {
+        res.json(table[i]);
+        return;
+      }
+    }
+
+    return res.send("Not a valid path");
+  }
+  return res.json(table);
+});
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
+
